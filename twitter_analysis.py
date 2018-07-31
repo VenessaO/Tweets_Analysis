f0e7c2a8460a75d0d367fb3a8e5ac8868bf5d401
Kyra <kyparris07@gmail.com>
2018-07-31 13:44:26 -0400

    added histograms

--- twitter_analysis.py
+++ twitter_analysis.py
@@ -20,12 +20,35 @@ tweetFile.close()
     # for each tweet access the text. Use the text feild tweet.text
     # print polarity
 
+polarity = []
+subjectivity = []
+
 for tweet in tweetData:
     tb = TextBlob(tweet["text"])
-    print(tb.polarity)
-    if tb.polarity > 0:
-        print("positive")
-    elif tb.polarity == 0:
-        print("neutral")
-    elif tb.polarity < 0:
-        print("negative")
+    polarity.append(tb.polarity)
+
+average = sum(polarity) / len(polarity)
+print("The average polarity is:", average)
+
+plt.hist(polarity)
+plt.ylabel("Frequency")
+plt.title(r"Twitter Analysis")
+
+for tweet in tweetData:
+    tb = TextBlob(tweet["text"])
+    subjectivity.append(tb.subjectivity)
+    # print(tb.polarity)
+    # if tb.polarity > 0:
+    #     print("positive")
+    # elif tb.polarity == 0:
+    #     print("neutral")
+    # elif tb.polarity < 0:
+    #     print("negative")
+
+average = sum(subjectivity) / len(subjectivity)
+print("The average subjectivity is:", average)
+
+plt.hist(subjectivity)
+plt.ylabel("Frequency")
+plt.title(r"Twitter Analysis")
+plt.show()
