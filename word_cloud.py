6fa0077b03940b94b645d0f0bfbf67fc90dcfcdb
Nessa <nessaonio@gmail.com>
2018-07-31 15:44:15 -0400

    list of words

--- word_cloud.py
+++ word_cloud.py
@@ -12,5 +12,36 @@ tweets = []
 
 for tweet in tweetData:
     tweets.append(tweet["text"])
+    break
 
-total = concatenate(tweets)
+result = ""
+for word in tweets:
+    result += str(word)
+
+data = result.split()
+
+words = []
+
+for word in data:
+    words.append(word)
+
+print(words)
+
+# print(result)
+
+# tb = TextBlob(result)
+#
+# filtered_words = [""]
+#
+# common_words = ["about", "when", "http"]
+#
+# for word in result:
+#     if len(word) <= 3 or word in common_words:
+#         filtered_words.append(word)
+#         continue
+#     # elif len(word) > 3:
+#         # unfiltered_words.append(word)
+#         # print(word)
+#
+# print(len(filtered_words))
+# print(len(result))
